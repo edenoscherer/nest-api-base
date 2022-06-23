d97570c9e4ee755774f7fdf30400307dba144860
Edeno Scherer <edenoscherer@gmail.com>
2022-06-22 23:20:50 -0300

    feat: config mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,6 +9,12 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       cache: true,
     }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=${process.env.MONGO_AUTH_SOURCE}`,
+      {
+        useNewUrlParser: true,
+      },
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
